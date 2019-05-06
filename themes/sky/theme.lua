450ff10dce73491bca4fcd2b5dc33a94edd14aa5
Jonatas <jonatasgb@gmail.com>
2019-05-06 19:57:13 -0300

    Update theme.lua

--- themes/sky/theme.lua
+++ themes/sky/theme.lua
@@ -29,8 +29,8 @@ theme.fg_minimize   = "#bdbdbd"
 theme.fg_square     = "#ff0066"
 theme.fg_squareu    = "#ffcc00"
 
-theme.useless_gap   = dpi(2)
-theme.border_width  = dpi(1)
+theme.useless_gap   = dpi(0)
+theme.border_width  = dpi(2)
 theme.border_normal = "#50505000"
 theme.border_focus  = "#00afaf"
 theme.border_marked = "#00adee"
