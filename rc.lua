7412de02eeb4ab1eebae9dda3a1f2af05137408c
quebravel <jonatasgb@gmail.com>
2025-03-09 19:16:12 -0300

    iniciando pasta

--- rc.lua
+++ rc.lua
@@ -18,6 +18,11 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
+
+local volume_widget = require("awesome-wm-widgets.volume-widget.volume")
+local calendar_widget = require("awesome-wm-widgets.calendar-widget.calendar")
+local fs_widget = require("awesome-wm-widgets.fs-widget.fs-widget")
+
 require("awful.hotkeys_popup.keys")
 require("collision")()
 
@@ -146,8 +151,23 @@ mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- {{{ Wibar
 -- Create a textclock widget
-mytextclock = wibox.widget.textclock(" %A %d de %b %H:%M ")
-
+mytextclock = wibox.widget.textclock()
+-- or customized
+local cw = calendar_widget({
+	theme = "light",
+	placement = "top_right",
+	start_sunday = false,
+	radius = 8,
+	-- with customized next/previous (see table above)
+	previous_month_button = 1,
+	next_month_button = 3,
+})
+mytextclock:connect_signal("button::press", function(_, _, _, button)
+	if button == 1 then
+		cw.toggle()
+	end
+end)
+---
 -- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
 	awful.button({}, 1, function(t)
@@ -261,6 +281,10 @@ awful.screen.connect_for_each_screen(function(s)
 		s.mytasklist, -- Middle widget
 		{ -- Right widgets
 			layout = wibox.layout.fixed.horizontal,
+			fs_widget(), --default
+			volume_widget({ -- modifiquei aqui
+				widget_type = "arc",
+			}),
 			mykeyboardlayout,
 			wibox.widget.systray(),
 			mytextclock,
