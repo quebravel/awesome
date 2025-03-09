6b597b2dc09e554a3967d6ecdf25f2e6c851ea95
Jonatas <jonatasgb@gmail.com>
2025-03-09 14:09:28 -0300

    Update rc.lua

--- rc.lua
+++ rc.lua
@@ -209,7 +209,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "bottom", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s }) -- bottom
 
     -- Add widgets to the wibox
     s.mywibox:setup {
