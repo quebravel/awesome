de58f4567e8de07e9a71d864b37c648dc989e815
quebravel <jonatasgb@gmail.com>
2025-03-24 19:33:24 -0300

    novos botoes para esconter/mostrar barra de titulo e wibar

--- rc.lua
+++ rc.lua
@@ -435,6 +435,17 @@ globalkeys = gears.table.join(
 	awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
 	awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
 
+  -- Show/hide wibox
+  awful.key({ modkey }, "b", function ()
+          for s in screen do
+              s.mywibox.visible = not s.mywibox.visible
+              if s.mybottomwibox then
+                  s.mybottomwibox.visible = not s.mybottomwibox.visible
+              end
+          end
+      end,
+      {description = "toggle wibox", group = "awesome"}),
+
 	-- modifiquei aqui
 	awful.key({ "Ctrl", altkey }, "l", function()
 		awful.tag.incmwfact(0.05)
@@ -496,6 +507,9 @@ globalkeys = gears.table.join(
 )
 
 clientkeys = gears.table.join(
+	awful.key({ modkey, 'Control' }, 't',
+		awful.titlebar.toggle,
+		{description = 'toggle title bar', group = 'client'}),
 	awful.key({ modkey }, "f", function(c)
 		c.fullscreen = not c.fullscreen
 		c:raise()
