80dc2337640ba3e3778cc16db5626bd68e03cd2e
quebravel <jonatasgb@gmail.com>
2025-03-25 20:12:51 -0300

    update

---
+++ install_awesomerc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone --recurse-submodules -j8 https://github.com/quebravel/awesome.git ~/.config/awesome
+
+sudo pacman -S alsa-utils pasystray python-gobject gammastep network-manager-applet
+
+sudo systemctl enable NetworkManager.service
+
+(git clone https://github.com/horst3180/arc-icon-theme --depth 1 ~/arc-icon-theme && cd ~/arc-icon-theme && ./autogen.sh --prefix=/usr sudo make install && rm -rf ~/arc-icon-theme)
+
+wget https://raw.githubusercontent.com/rxi/json.lua/refs/heads/master/json.lua -P ~/.config/awesome/
+
--- rc.lua
+++ rc.lua
@@ -95,7 +95,7 @@ awful.layout.layouts = {
 	-- awful.layout.suit.corner.ne,
 	-- awful.layout.suit.corner.sw,
 	-- awful.layout.suit.corner.se,
-	bling.layout.mstab,
+	-- bling.layout.mstab,
 	bling.layout.centered,
 	bling.layout.vertical,
 	bling.layout.horizontal,
