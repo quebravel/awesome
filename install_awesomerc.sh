8dba788aa8125503e238c6e56e1ecc231b6e7ecd
quebravel <jonatasgb@gmail.com>
2025-03-25 20:30:44 -0300

    add picom and start networkmanager

--- install_awesomerc.sh
+++ install_awesomerc.sh
@@ -2,9 +2,10 @@
 
 git clone --recurse-submodules -j8 https://github.com/quebravel/awesome.git ~/.config/awesome
 
-sudo pacman -S alsa-utils pasystray python-gobject gammastep network-manager-applet
+sudo pacman -S alsa-utils pasystray python-gobject picom gammastep network-manager-applet
 
 sudo systemctl enable NetworkManager.service
+sudo systemctl start NetworkManager.service
 
 (git clone https://github.com/horst3180/arc-icon-theme --depth 1 ~/arc-icon-theme && cd ~/arc-icon-theme && ./autogen.sh --prefix=/usr sudo make install && rm -rf ~/arc-icon-theme)
 
