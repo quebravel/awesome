37322948cc9c5d3249495ad1c6cf10a19568f518
quebravel <jonatasgb@gmail.com>
2025-04-23 20:43:55 -0300

    add feh floating

--- rc.lua
+++ rc.lua
@@ -669,6 +669,7 @@ awful.rules.rules = {
 				"veromix",
 				"xtightvncviewer",
 				"mpv",
+				"feh",
 			},
 
 			-- Note that the name property shown in xprop might be set slightly after creation of the client
